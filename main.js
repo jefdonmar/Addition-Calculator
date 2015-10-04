406a4f623c9a7d50fc5984c45363d54b61387155
Jeffrey Martín <jefdonmar@gmail.com>
2015-10-04 19:29:50 -0400

    Finished the normal mode and starting on the hard

---
+++ main.js
@@ -0,0 +1,114 @@
+//Multiplication Operations
+var multiplication = function (num_one, num_two) {
+  return parseInt(num_one) * parseInt(num_two);
+};
+
+//Grab Elements
+var num_one = document.querySelector('#num_one');
+var num_two = document.querySelector('#num_two');
+var multiplyMe = document.querySelector('#calculateBtn');
+var answerElem = document.querySelector('#answer');
+
+//Variable Declarations 
+var multiply1, multiply2, multiplyText;
+
+//Performing Addition Operation
+var calculate = function () {
+  multiply1 = num_one.value;
+  multiply2 = num_two.value;
+  multiplyText = multiplication(multiply1, multiply2);
+    answerElem.textContent = multiplyText; 
+};
+
+//What happens when the button is clicked 
+multiplyMe.addEventListener('click', calculate);
+
+
+//Division Operations
+var division = function (num_one, num_two) {
+  return parseInt(num_one) / parseInt(num_two);
+};
+
+//Grab Elements
+var num_one = document.querySelector('#num_one');
+var num_two = document.querySelector('#num_two');
+var divideMe = document.querySelector('#calculateBtn');
+var answerElem = document.querySelector('#answer');
+
+//Variable Declarations 
+var divide1, divide2, divideText;
+
+//Performing Addition Operation
+var calculate = function () {
+  divide1 = num_one.value;
+  divide2 = num_two.value;
+  divideText = division(divide1, divide2);
+    answerElem.textContent = divideText; 
+};
+
+//What happens when the button is clicked 
+divideMe.addEventListener('click', calculate);
+
+//Subtraction Operations
+var subtraction = function (num_one, num_two) {
+  return parseInt(num_one) - parseInt(num_two);
+};
+
+//Grab Elements
+var num_one = document.querySelector('#num_one');
+var num_two = document.querySelector('#num_two');
+var subMe = document.querySelector('#calculateBtn');
+var answerElem = document.querySelector('#answer');
+
+//Variable Declarations 
+var sub1, sub2, subText;
+
+//Performing Addition Operation
+var calculate = function () {
+  sub1 = num_one.value;
+  sub2 = num_two.value;
+  subText = subtraction(sub1, sub2);
+    answerElem.textContent = subText; 
+};
+
+//What happens when the button is clicked 
+subMe.addEventListener('click', calculate);
+
+
+//Addition Operations
+var addition = function (num_one, num_two) {
+  return parseInt(num_one) + parseInt(num_two);
+};
+
+//Grab Elements
+var num_one = document.querySelector('#num_one');
+var num_two = document.querySelector('#num_two');
+var addMe = document.querySelector('#calculateBtn');
+var answerElem = document.querySelector('#answer');
+
+//Variable Declarations 
+var add1, add2, additionText;
+
+//Performing Addition Operation
+var calculate = function () {
+  add1 = num_one.value;
+  add2 = num_two.value;
+  additionText = addition(add1, add2);
+    answerElem.textContent = additionText; 
+};
+
+//What happens when the button is clicked 
+addMe.addEventListener('click', calculate);
+
+
+
+
+
+
+
+
+
+
+
+
+
